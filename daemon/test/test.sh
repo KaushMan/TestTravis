fc3196768c4d43f2e63d23708f1a981ad45b37ba
KamdenChew <40366046+KamdenChew@users.noreply.github.com>
2019-02-18 14:50:35 -0800

    Update test.sh

--- daemon/test/test.sh
+++ daemon/test/test.sh
@@ -1,7 +1,9 @@
-echo "Initial directoru:"
+echo "Initial directory:"
 pwd
+echo "Subdirectories:"
+ls
 echo "After cd:"
-cd daemon/tests
+cd daemon/test
 pwd
 echo "Python3 version:"
 python3 --version
