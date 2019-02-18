1e1d72c714dad7721604071a7cfcdaac334f0c72
KamdenChew <40366046+KamdenChew@users.noreply.github.com>
2019-02-18 15:23:09 -0800

    Update test.sh

--- daemon/test/test.sh
+++ daemon/test/test.sh
@@ -2,8 +2,29 @@ echo "Initial directory:"
 pwd
 echo "Subdirectories:"
 ls
-echo "After cd:"
-cd daemon/test
 pwd
 echo "Python3 version:"
 python3 --version
+
+#Install GitPython and inotify
+sudo pip install GitPython
+sudo pip install inotify
+
+#Create gitup files
+cd /usr/share/
+sudo mkdir gitup
+cd gitup
+sudo touch repositories.csv
+sudo cat "local_path,last_pulled \n /tmp/testrepo,0" > repositories.csv
+
+cd /tmp
+mkdir testrepo
+cd testrepo
+git init
+
+#Start the daemon
+cd /home/travis/build/KamdenChew/TestTravis/daemon
+python3.7 start_daemon.py
+
+#Stop the daemon
+python3.7 stop_daemon.py
