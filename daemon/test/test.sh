5fa21274bc9e9ad3f18d005c4a62752e7dcf42cb
KamdenChew <40366046+KamdenChew@users.noreply.github.com>
2019-02-18 16:36:23 -0800

    Update test.sh

--- daemon/test/test.sh
+++ daemon/test/test.sh
@@ -1,16 +1,13 @@
 #Install GitPython and inotify
-echo "Pip3 version:"
-pip3.7 --version
-sudo pip3.7 install GitPython
-sudo pip3.7 install inotify
+sudo pip install GitPython
+sudo pip install inotify
 
 #Create gitup files
 cd /usr/share/
 sudo mkdir gitup
 cd gitup
-echo "initializing repositories.csv"
 sudo touch repositories.csv
-echo "initialized repositories.csv"
+sudo chmod +w repositories.csv
 sudo cat "local_path,last_pulled \n /tmp/testrepo,0" > repositories.csv
 
 cd /tmp
