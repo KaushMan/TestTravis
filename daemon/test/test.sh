fdf06122b95d8a6e2ea5e6968ba5602e0a55b50b
KamdenChew <40366046+KamdenChew@users.noreply.github.com>
2019-02-18 17:49:34 -0800

    Update test.sh

--- daemon/test/test.sh
+++ daemon/test/test.sh
@@ -8,7 +8,8 @@ sudo mkdir gitup
 cd gitup
 sudo touch repositories.csv
 sudo chmod +w repositories.csv
-sudo cat "local_path,last_pulled \n /tmp/testrepo,0" > repositories.csv
+#sudo cat "local_path,last_pulled \n /tmp/testrepo,0" > repositories.csv
+sudo ls cat "local_path,last_pulled \n /tmp/testrepo,0" | sudo tee repositories.csv > /dev/null
 
 cd /tmp
 mkdir testrepo
